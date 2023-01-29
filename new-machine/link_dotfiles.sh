f2485afa374b41bcfbac81aea7dfb3ffe990d3ea
Carly Wooten <104537084+carly-kins@users.noreply.github.com>
2023-01-29 00:44:16 -0600

    made changes after testing on new machine

--- new-machine/link_dotfiles.sh
+++ new-machine/link_dotfiles.sh
@@ -6,6 +6,8 @@ DEV="dev/.oh-my-zsh-custom"
 ln -s $NEW/.bash_profile ~
 ln -s $NEW/.git-prompt ~
 
+rm .zshrc
+
 ln -s $DEV ~
 ln -s $DEV/templates/zshrc.zsh-template ~/.zshrc
 #ln -s $DEV/.dircolors ~/.dircolors
