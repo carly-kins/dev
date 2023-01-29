0d8632531e40996216e88e7c7697bdfe53afdb29
Carly Wooten <cwooten@reingold.com>
2023-01-29 14:17:25 -0500

    update to ignore theme in git

--- new-machine/link_dotfiles.sh
+++ new-machine/link_dotfiles.sh
@@ -11,3 +11,5 @@ rm .zshrc
 ln -s $DEV ~
 ln -s $DEV/templates/zshrc.zsh-template ~/.zshrc
 #ln -s $DEV/.dircolors ~/.dircolors
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
