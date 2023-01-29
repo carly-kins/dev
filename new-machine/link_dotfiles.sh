e276ac719bee3c4e1ed6ebe5f6b2a92f69c9c680
Carly Wooten <104537084+carly-kins@users.noreply.github.com>
2023-01-29 13:48:45 -0600

    add plugins to dotfiles

--- new-machine/link_dotfiles.sh
+++ new-machine/link_dotfiles.sh
@@ -11,3 +11,9 @@ rm .zshrc
 ln -s $DEV ~
 ln -s $DEV/templates/zshrc.zsh-template ~/.zshrc
 #ln -s $DEV/.dircolors ~/.dircolors
+
+#update plugins
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
